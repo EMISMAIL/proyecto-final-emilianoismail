d5625830413dbb1243d8dce10e66c64d4ba80de6
Emiliano Ismail <emilianoismail93@gmail.com>
2023-01-09 20:58:03 -0300

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+var buttonText = document.getElementById('button-text')
+buttonText.addEventListener("click", clicked)
+function clicked() {
+    alert('Busco un trabajo fulltime, con posibilidades de crecer y capacitarme. Soy un persona muy leal, con grandes cualidades adaptativas y dedico todo mis esfuerzos a fin de lograr los objetivos propuestos')
+    
+}
+
